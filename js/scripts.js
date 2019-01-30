83aaad75435a792e6e287ab76377c5ae698ee040
jaredh123 <46456502+jaredh123@users.noreply.github.com>
2019-01-30 08:07:53 -0800

    Add player signaling and rules

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
   var pointsOne = 0;
   var pointsTwo = 0;
 
@@ -13,6 +12,8 @@ $(document).ready(function() {
 
   $("button#roll").click(function() {
     if (turn == 1){
+      $("#player2").removeClass("blue");
+      $("#player1").addClass("blue");
       roll = Math.floor((Math.random() * 6) + 1);
       runningTotal = runningTotal + roll;
       $("#rollValue").text(roll);
@@ -20,14 +21,20 @@ $(document).ready(function() {
       if (roll == 1) {
         turn = 2;
         runningTotal = 0;
+        $("#player1").removeClass("blue");
+        $("#player2").addClass("blue");
         $("#turn").text("Its player " + turn + "'s turn");
+        alert("Its player " + turn + "'s turn");
       }
       if (pointsOne >= 100) {
         alert("Player 1 Wins");
         location.reload();
+        $("#rules").hide();
       }
     }
     else if (turn == 2) {
+      $("#player1").removeClass("blue");
+      $("#player2").addClass("blue");
       roll = Math.floor((Math.random() * 6) + 1);
       runningTotal = runningTotal + roll;
       $("#rollValue").text(roll);
@@ -35,22 +42,28 @@ $(document).ready(function() {
       if (roll == 1) {
         turn = 1;
         runningTotal = 0;
+        $("#player2").removeClass("blue");
+        $("#player1").addClass("blue");
         $("#turn").text("Its player " + turn + "'s turn");
+        alert("Its player " + turn + "'s turn");
       }
       if (pointsTwo >= 100) {
         alert("Player 2 Wins");
         location.reload();
+        $("#rules").hide();
       }
     }
   });
 
-
-  $("button#switch").click(function(){
+  $("button#switch").click(function() {
     if (turn == 1){
+      $("#player1").removeClass("blue");
+      $("#player2").addClass("blue");
       $("#turn").text("Its player 2's turn");
       pointsOne = pointsOne + runningTotal;
       $("#pointsOne").text(pointsOne);
       turn++;
+      alert("Its player " + turn + "'s turn");
       runningTotal = 0;
       if (pointsOne >= 100) {
         alert("Player 1 Wins");
@@ -58,15 +71,22 @@ $(document).ready(function() {
       }
     }
     else {
+      $("#player2").removeClass("blue");
+      $("#player1").addClass("blue");
       $("#turn").text("Its player 1's turn");
       pointsTwo = pointsTwo + runningTotal;
       $("#pointsTwo").text(pointsTwo);
       turn = 1;
+      alert("Its player " + turn + "'s turn");
       runningTotal = 0;
       if (pointsTwo >= 100) {
         alert("Player 2 Wins");
         location.reload();
       }
     }
+  });
+
+  $("#ruleButton").click(function() {
+    $("#rules").toggle();
   });
 });
